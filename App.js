4a851523f3816b83cbcac6f213a189b055ff2681
eztasterwp <sidmandimas@gmail.com>
2024-06-19 21:55:00 +0100

    Add files via upload

--- App.js
+++ App.js
@@ -2,18 +2,25 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faExchangeAlt, faHammer, faUserFriends, faHandHoldingUsd, faCoins, faEllipsisH } from '@fortawesome/free-solid-svg-icons';
+import Mine from './Mine';
+import Friends from './Friends';
+import Earn from './Earn';
+import Airdrop from './Airdrop';
 
 function App() {
-  const [points, setPoints] = useState(0);
+  const [points, setPoints] = useState(100); // текущие доступные очки
+  const [totalPoints, setTotalPoints] = useState(100); // общее количество заработанных очков
   const [messages, setMessages] = useState([]);
   const [backgroundLoaded, setBackgroundLoaded] = useState(false);
   const [level, setLevel] = useState(1);
   const [coinsPerTap, setCoinsPerTap] = useState(2);
-  const [coinsToLevelUp, setCoinsToLevelUp] = useState(50); // Уменьшил для тестирования
+  const [coinsToLevelUp, setCoinsToLevelUp] = useState(calculateCoinsToLevelUp(1));
   const [activeButton, setActiveButton] = useState('exchange');
   const [username, setUsername] = useState('User');
   const [levelUpNotification, setLevelUpNotification] = useState('');
-
+  const [showModal, setShowModal] = useState(false);
+  const [currentQuest, setCurrentQuest] = useState({});
+  
   useEffect(() => {
     const img = new Image();
     img.src = 'background.png';
@@ -34,26 +41,32 @@ function App() {
       }
     };
 
-    const preventCloseSwipe = (e) => {
-      if (e.changedTouches[0].clientY > window.innerHeight - 50) {
-        e.preventDefault();
+    const allowSwipeOnMenu = (e) => {
+      if (e.target.closest('.buttons-container')) {
+        return; // Разрешаем свайп на меню
       }
+      e.preventDefault();
     };
 
-    document.addEventListener('touchstart', preventSwipe, { passive: false });
-    document.addEventListener('touchmove', preventSwipe, { passive: false });
-    document.addEventListener('touchend', preventCloseSwipe, { passive: false });
+    if (activeButton === 'exchange') {
+      document.addEventListener('touchstart', allowSwipeOnMenu, { passive: false });
+      document.addEventListener('touchmove', allowSwipeOnMenu, { passive: false });
+    } else {
+      document.removeEventListener('touchstart', allowSwipeOnMenu);
+      document.removeEventListener('touchmove', allowSwipeOnMenu);
+    }
 
     return () => {
-      document.removeEventListener('touchstart', preventSwipe);
-      document.removeEventListener('touchmove', preventSwipe);
-      document.removeEventListener('touchend', preventCloseSwipe);
+      document.removeEventListener('touchstart', allowSwipeOnMenu);
+      document.removeEventListener('touchmove', allowSwipeOnMenu);
     };
-  }, []);
+  }, [activeButton]);
+
+  function calculateCoinsToLevelUp(currentLevel) {
+    return 500 + 2000 * (currentLevel - 1);
+  }
 
   const handleTouchStart = (event) => {
-    event.preventDefault();
-
     const plantElement = document.querySelector('.plant');
     const rect = plantElement.getBoundingClientRect();
 
@@ -69,9 +82,12 @@ function App() {
       ) {
         setPoints(prevPoints => {
           const newPoints = prevPoints + coinsPerTap;
+          setTotalPoints(prevTotalPoints => prevTotalPoints + coinsPerTap);
+
           if (newPoints >= coinsToLevelUp) {
             setLevel(prevLevel => {
               const newLevel = prevLevel + 1;
+              setCoinsToLevelUp(calculateCoinsToLevelUp(newLevel));
               setLevelUpNotification(`Congratulations, you have reached level ${newLevel}, keep going - airdrop soon`);
               setTimeout(() => setLevelUpNotification(''), 3000); // Уведомление исчезает через 3 секунды
               return newLevel;
@@ -104,6 +120,20 @@ function App() {
     setActiveButton(buttonId);
   };
 
+  const handleQuestClick = (questTitle, questCost) => {
+    setCurrentQuest({ title: questTitle, cost: questCost });
+    setShowModal(true);
+  };
+
+  const handleConfirmPurchase = () => {
+    if (points >= currentQuest.cost) {
+      setPoints(points - currentQuest.cost);
+      setShowModal(false);
+    } else {
+      alert('Недостаточно очков для выполнения квеста.');
+    }
+  };
+
   const formatPoints = (points) => {
     if (points >= 1000000) {
       return (points / 1000000).toFixed(1) + 'M';
@@ -115,9 +145,25 @@ function App() {
     return (points / coinsToLevelUp) * 100;
   };
 
-  const handleTouchEnd = (event) => {
-    // Эта функция остановит длительное нажатие и заставит событие "отпустить" пальцы
-    event.preventDefault();
+  const renderContent = () => {
+    switch (activeButton) {
+      case 'exchange':
+        return (
+          <div className="plant-container">
+            <div className="plant"></div>
+          </div>
+        );
+      case 'mine':
+        return <Mine onQuestClick={handleQuestClick} />;
+      case 'friends':
+        return <Friends />;
+      case 'earn':
+        return <Earn />;
+      case 'airdrop':
+        return <Airdrop />;
+      default:
+        return null;
+    }
   };
 
   if (!backgroundLoaded) {
@@ -125,7 +171,7 @@ function App() {
   }
 
   return (
-    <div className="App" onTouchStart={handleTouchStart} onTouchEnd={handleTouchEnd} onTouchMove={(e) => e.preventDefault()}>
+    <div className="App" onTouchStart={handleTouchStart}>
       <div className="header">
         <div className="header-top">
           <div className="header-col" style={{ display: 'flex', alignItems: 'center' }}>
@@ -136,22 +182,27 @@ function App() {
             <FontAwesomeIcon icon={faEllipsisH} className="settings-icon" />
           </div>
         </div>
-        <div className="header-bottom">
-          <div className="coin-display">
-            <img src="coin.png" alt="coin" className="coin" />
-            <h1>{formatPoints(points)}</h1>
-          </div>
-          <div className="level-display">
-            <div className="level-bar-container">
-              <div className="level-bar" style={{ width: `${calculateLevelProgress()}%` }}></div>
+        {activeButton === 'exchange' && (
+          <div className="header-bottom">
+            <div className="coin-display">
+              <img src="coin.png" alt="coin" className="coin" />
+              <h1>{formatPoints(points)}</h1>
             </div>
-            <div className="level-text">Grower {level}/10</div>
+            <div className="level-display">
+              <div className="level-bar-container">
+                <div className="level-bar" style={{ width: `${calculateLevelProgress()}%` }}></div>
+              </div>
+              <div className="level-text">Grower {level}/100000</div>
+            </div>
           </div>
-        </div>
+        )}
       </div>
-      <div className="plant-container">
-        <div className="plant"></div>
+      <div className="stats-display">
+        <span>Level: {level}</span>
+        <span>Points: {points}</span>
+        <span>Total Points: {totalPoints}</span>
       </div>
+      {renderContent()}
       <div className="buttons-container">
         <div className={`button ${activeButton === 'exchange' ? 'active' : ''}`} id="exchange" onClick={() => handleButtonClick('exchange')}>
           <FontAwesomeIcon icon={faExchangeAlt} />
@@ -188,6 +239,15 @@ function App() {
       {levelUpNotification && (
         <div className="level-up-notification">
           {levelUpNotification}
+        </div>
+      )}
+      {showModal && (
+        <div className="modal">
+          <div className="modal-content">
+            <p>Купить {currentQuest.title} за {currentQuest.cost} очков?</p>
+            <button onClick={handleConfirmPurchase}>Да</button>
+            <button onClick={() => setShowModal(false)}>Нет</button>
+          </div>
         </div>
       )}
     </div>

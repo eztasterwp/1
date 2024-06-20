e542a0b332af9ae6c0e5fc99914a02edfd294bf7
eztasterwp <sidmandimas@gmail.com>
2024-06-20 15:48:27 +0100

    Add files via upload

--- src/Mine.js
+++ src/Mine.js
@@ -24,15 +24,15 @@ function Mine({ onQuestPurchase, points, quests }) {
         {quests.map((quest) => (
           <div className="quest" key={quest.id} onClick={() => handleQuestPurchase(quest)}>
             <div className="quest-header">
-              <div className="quest-title">
-                <FontAwesomeIcon icon={quest.icon} className="quest-icon" /> {quest.title}
-              </div>
-              <div className="quest-profit">Profit per hour: <span>+{quest.income}</span></div>
+              <FontAwesomeIcon icon={quest.icon} className="quest-icon" />
+              <div className="quest-title">{quest.title}</div>
+              <div className="quest-level">lvl {quest.level}</div>
             </div>
+            <div className="quest-profit">Profit per hour: +{quest.income}</div>
             <div className="quest-details">
-              <div className="quest-level">Level: {quest.level}</div>
               <div className="quest-cost">
-                <span>{quest.cost} <FontAwesomeIcon icon={faCoins} /></span>
+                {quest.cost}
+                <FontAwesomeIcon icon={faCoins} />
               </div>
             </div>
           </div>

5d9305620e6e377bfa7909c6b061e5a2723a2575
eztasterwp <sidmandimas@gmail.com>
2024-06-20 11:04:00 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -122,7 +122,7 @@ function App() {
           setMessages(prevMessages =>
             prevMessages.filter(msg => msg.id !== newMessage.id)
           );
-        }, 1000); // Ускоряем анимацию до 1 секунды
+        }, 2000); // Увеличиваем анимацию до 2 секунд
       }
     });
   };

f7269e3437b8426c21c993d133daf97e1bf9f7c3
eztasterwp <sidmandimas@gmail.com>
2024-06-21 13:17:20 +0100

    Add files via upload

--- src/Mine.js
+++ src/Mine.js
@@ -1,62 +1,13 @@
 import React from 'react';
-import './Mine.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCoins } from '@fortawesome/free-solid-svg-icons';
-import seedsImage from './images/seeds.png';
-import soilImage from './images/soil.png';
-import fertilizerImage from './images/fertilizer.png';
-import taxImage from './images/tax.png';
-import deliveryImage from './images/delivery.png';
-import licenseImage from './images/license.png';
+import './Mine.css'; // Импорт стилей для Mine
 
-const questBackgrounds = {
-  'Купить семена': seedsImage,
-  'Купить грунт': soilImage,
-  'Купить удобрения': fertilizerImage,
-  'Заплатить налоги': taxImage,
-  'Отправить товар в CoffeeShop': deliveryImage,
-  'Купить лицензию': licenseImage,
-};
-
-function Mine({ onQuestPurchase, points, quests }) {
-  const handleQuestPurchase = (quest) => {
-    if (points >= quest.cost) {
-      onQuestPurchase(quest.id);
-    } else {
-      alert('Недостаточно очков для выполнения квеста.');
-    }
-  };
-
+const Mine = () => {
   return (
     <div className="mine-page">
-      <div className="mine-background"></div> {/* Полупрозрачный и размытый фон */}
-      <div className="mine-container">
-        <div className="quests">
-          {quests.map((quest) => (
-            <div
-              className="quest"
-              key={quest.id}
-              onClick={() => handleQuestPurchase(quest)}
-              style={{ backgroundImage: `url(${questBackgrounds[quest.title]})` }}
-            >
-              <div className="quest-content">
-                <div className="quest-level">
-                  <FontAwesomeIcon icon={quest.icon} className="quest-icon" />
-                  lvl {quest.level}
-                </div>
-                <div className="quest-title">{quest.title}</div>
-                <div className="quest-cost">
-                  {quest.cost}
-                  <FontAwesomeIcon icon={faCoins} />
-                </div>
-              </div>
-              <div className="quest-profit">Profit per hour: +{quest.income}</div>
-            </div>
-          ))}
-        </div>
-      </div>
+      <h1>Mine</h1>
+      <p>Здесь будет информация о майнинге.</p>
     </div>
   );
-}
+};
 
 export default Mine;
